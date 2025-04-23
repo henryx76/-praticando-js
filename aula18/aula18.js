8294dd0f16b6a256401dd89bbb54695b26fe47f0
Alanna <henryhesdras349@gmail.com>
2025-04-23 17:06:09 -0300

    aula18

--- aula18/aula18.js
+++ aula18/aula18.js
@@ -1,73 +1,70 @@
-//COMANDO BASE PARA PEGAR DATA
+// COMANDO BASE PARA PEGAR DATA
 let data = new Date();
-//console.log(data);
 
-//PEGAR O ANO ATUAL COM 4 DIGITOS
+// PEGAR O ANO ATUAL COM 4 DIGITOS
 let ano = data.getFullYear();
-console.log(ano);
+console.log("Ano:", ano);
 
-//PEGAR O MES ATUAL - DE 0 ATÉ 11 SENDO 0 JANEIRO 11 DEZEMBRO
+// PEGAR O MÊS ATUAL - DE 0 ATÉ 11 (0 = JANEIRO, 11 = DEZEMBRO)
 let mes = data.getMonth();
-//console.log(mes);
 
-//MOSTRAR O MÊS NO FORMATO ESCRITO
-const mesDoAno =  ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"]
-let mesEscrito = mesesDoAno[data.getMonth()];
+// MOSTRAR O MÊS NO FORMATO ESCRITO
+const mesesDoAno = ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"];
+let mesEscrito = mesesDoAno[mes];
+console.log("Mês escrito:", mesEscrito);
 
-//PEGAR DIA DO MÊS - 1 ATÉ 31
+// PEGAR DIA DO MÊS - 1 ATÉ 31
 let diaMes = data.getDate();
+console.log("Dia do mês:", diaMes);
 
-//PEGAR O DIA DA SEMANA - 0 ATÉ 6
+// PEGAR O DIA DA SEMANA - 0 ATÉ 6
 let diaSemana = data.getDay();
+const diasDaSemana = ["domingo", "segunda", "terça", "quarta", "quinta", "sexta", "sábado"];
+let diaSemanaEscrito = diasDaSemana[diaSemana];
+console.log("Dia da semana:", diaSemanaEscrito);
 
-const diaDaSemana = ["Domingo", "segunda", "terça", "quarta", "quinta", "quinta", "sexta", "sabado"]
-let diaSemanaEscrito = diaSemana[data.getDay()]
-
-//PEGAR A HORA - DE 0 ATÉ 23
+// PEGAR A HORA
 let hora = data.getHours();
-console.log(hora)
+console.log("Hora:", hora);
 
-//PEGAR O MINUTOS - DE 0 ATÉ 59
+// PEGAR MINUTOS
 let minutos = data.getMinutes();
+console.log("Minutos:", minutos);
 
-//PEGAR SEGUNDOS - DE 0 ATÉ 59
+// PEGAR SEGUNDOS
 let segundos = data.getSeconds();
-console.log(segundos);
+console.log("Segundos:", segundos);
 
-//PEGAR MILISEGUNDOS - 0 ATÉ 999
+// PEGAR MILISSEGUNDOS
 let milisegundos = data.getMilliseconds();
-console.log(milisegundos);
+console.log("Milissegundos:", milisegundos);
 
-//PEGAR A DATA NO PADRÃO BRASILEIRO - DIA / MES/ ANO
-let dataBR = data.toLocaleDateString('pt-BR', {datestyle: 'short'});
-console.log(dataBR);
+// PEGAR A DATA NO PADRÃO BRASILEIRO
+let dataBR = data.toLocaleDateString('pt-BR', { dateStyle: 'short' });
+console.log("Data BR:", dataBR);
 
-//PEGAR OS VALORES SEPARADOS
-d = new Date();
-diaMes = d.Date();
-mes = d.getmonth() + 1;
-ano = d.getFullYear();
-function addZero(x) { return x < 10 ? '0' + x : '' +x; }
-
-let dataPadraoBR = diaMes + "/" + mes + "/" + ano;
-console.log(dataPadraoBR);
-
-//COMPARAR DATAS - MAIOR OU MENOR. EX: VENCIMENTOS
-var hoje = new date();
-var vencimento = new date(2024, 0, 15);
-
-if(hoje > vencimento){
-    console.log("sua conta está vencida");
-}else{
-    console.log("ainda não venceu")
+// PEGAR OS VALORES SEPARADOS
+function addZero(x) {
+  return x < 10 ? '0' + x : '' + x;
 }
 
-//DIFERENÇA ENTRE DUAS DATAS EM DIAS
-var dataInicial = new date();
-var dataFinal = new date(2024, 11, 31)
+let dataFormatada = addZero(diaMes) + "/" + addZero(mes + 1) + "/" + ano;
+console.log("Data formatada:", dataFormatada);
 
-var diferencaTempo = dataFinal.getTime() - dataInicial.getTime();
+// COMPARAR DATAS - MAIOR OU MENOR
+let hoje = new Date();
+let vencimento = new Date(2024, 0, 15); // Janeiro = 0
 
-var diferencaDias = Math.ceil(diferencaTempo / (24 * 60 * 60 *1000));
+if (hoje > vencimento) {
+  console.log("Sua conta está vencida");
+} else {
+  console.log("Ainda não venceu");
+}
 
-console.log(diferencaDias + " dias");
+// DIFERENÇA ENTRE DUAS DATAS EM DIAS
+let dataInicial = new Date();
+let dataFinal = new Date(2024, 11, 31); // Dezembro = 11
+
+let diferencaTempo = dataFinal.getTime() - dataInicial.getTime();
+let diferencaDias = Math.ceil(diferencaTempo / (24 * 60 * 60 * 1000));
+console.log("Faltam", diferencaDias, "dias");
